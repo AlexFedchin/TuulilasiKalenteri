e0e496a481fc787ce3f1be60ba9e6989c474bbba
Alexander <alexanderfedchin@gmail.com>
2025-04-07 16:53:31 +0300

    Some initial project settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -23,25 +21,13 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-        <h2>Message from server:</h2>
-        <p>{message}</p>
-        <p>
-          If you see this message, the server is running and the proxy is
-          working!
-        </p>
-      </div>
-    </>
+    <div>
+      <h2>Message from server:</h2>
+      <p>{message}</p>
+      <p>
+        If you see this message, the server is running and the proxy is working!
+      </p>
+    </div>
   );
 }
 
