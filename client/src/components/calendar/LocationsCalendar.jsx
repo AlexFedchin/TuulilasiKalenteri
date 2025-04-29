f845788ef0ea1f831df65242327e75e4e2f02cd3
Alexander <alexanderfedchin@gmail.com>
2025-04-29 10:56:15 +0300

    Fix loader in locations calendar

--- client/src/components/calendar/LocationsCalendar.jsx
+++ client/src/components/calendar/LocationsCalendar.jsx
@@ -27,6 +27,7 @@ const LocationsCalendar = ({
   const [bookings, setBookings] = useState([]);
   const [filteredBookings, setFilteredBookings] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [locationsLoading, setLocationsLoading] = useState(true);
   const [locations, setLocations] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedLocation, setSelectedLocationState] = useState(null);
@@ -81,6 +82,8 @@ const LocationsCalendar = ({
         setLocations(data);
       } catch (error) {
         console.error("Error fetching locations:", error);
+      } finally {
+        setLocationsLoading(false);
       }
     };
 
@@ -221,104 +224,115 @@ const LocationsCalendar = ({
           </TableRow>
         </TableHead>
         <TableBody sx={{ position: "relative" }}>
-          {times.map((time) => (
-            <TableRow key={time}>
-              <TableCell
-                sx={{
-                  py: 0.5,
-                  pr: 0.5,
-                  pl: 0,
-                  height: "40px",
-                  boxSizing: "border-box",
-                  border: "none",
-                }}
-              >
-                <Typography
-                  variant="body2"
-                  sx={{ color: "var(--off-black)", textAlign: "right" }}
+          {!locationsLoading &&
+            times.map((time) => (
+              <TableRow key={time}>
+                <TableCell
+                  sx={{
+                    py: 0.5,
+                    pr: 0.5,
+                    pl: 0,
+                    height: "40px",
+                    boxSizing: "border-box",
+                    border: "none",
+                  }}
                 >
-                  {time}
-                </Typography>
-              </TableCell>
-              {locations.map((location) => {
-                const bookingsForSlot = getBookingForTimeSlot(time, location);
-                const widthNum =
-                  bookingsForSlot.length > 0
-                    ? 100.0 / bookingsForSlot.length
-                    : 100;
-                const bookingWidth = `${widthNum}%`;
-
-                return (
-                  <TableCell
-                    key={location._id}
-                    sx={{
-                      border: "none",
-                      p: 0.5,
-                      height: "40px",
-                      boxSizing: "border-box",
-                    }}
+                  <Typography
+                    variant="body2"
+                    sx={{ color: "var(--off-black)", textAlign: "right" }}
                   >
-                    <Box
-                      onClick={() =>
-                        isValidCell(bookingsForSlot)
-                          ? handleSlotClick(time, location._id)
-                          : undefined
-                      }
+                    {time}
+                  </Typography>
+                </TableCell>
+                {locations.map((location) => {
+                  const bookingsForSlot = getBookingForTimeSlot(time, location);
+                  const widthNum =
+                    bookingsForSlot.length > 0
+                      ? 100.0 / bookingsForSlot.length
+                      : 100;
+                  const bookingWidth = `${widthNum}%`;
+
+                  return (
+                    <TableCell
+                      key={location._id}
                       sx={{
-                        position: "relative",
-                        cursor: isValidCell(bookingsForSlot)
-                          ? "pointer"
-                          : "default",
-                        bgcolor: isValidCell(bookingsForSlot)
-                          ? "var(--white)"
-                          : "var(--light-grey)",
-                        borderRadius: 2,
-                        boxSizing: "border-box",
-                        height: "100%",
-                        width: "100%",
-                        boxShadow: "0 0 8px rgba(0, 0, 0, 0.1)",
+                        border: "none",
                         p: 0.5,
-                        ...(isValidCell(bookingsForSlot) && {
-                          "&:hover": {
-                            bgcolor: "var(--white-onhover)",
-                          },
-                        }),
+                        height: "40px",
+                        boxSizing: "border-box",
                       }}
                     >
-                      {bookingsForSlot.map((booking, index) => {
-                        const left = `${index * widthNum}%`;
-                        return (
-                          <BookingBox
-                            booking={booking}
-                            left={left}
-                            width={bookingWidth}
-                            onClick={handleBookingBoxClick}
-                            key={booking._id}
-                          />
-                        );
-                      })}
-                    </Box>
-                  </TableCell>
-                );
-              })}
-            </TableRow>
-          ))}
+                      <Box
+                        onClick={() =>
+                          isValidCell(bookingsForSlot)
+                            ? handleSlotClick(time, location._id)
+                            : undefined
+                        }
+                        sx={{
+                          position: "relative",
+                          cursor: isValidCell(bookingsForSlot)
+                            ? "pointer"
+                            : "default",
+                          bgcolor: isValidCell(bookingsForSlot)
+                            ? "var(--white)"
+                            : "var(--light-grey)",
+                          borderRadius: 2,
+                          boxSizing: "border-box",
+                          height: "100%",
+                          width: "100%",
+                          boxShadow: "0 0 8px rgba(0, 0, 0, 0.1)",
+                          p: 0.5,
+                          ...(isValidCell(bookingsForSlot) && {
+                            "&:hover": {
+                              bgcolor: "var(--white-onhover)",
+                            },
+                          }),
+                        }}
+                      >
+                        {bookingsForSlot.map((booking, index) => {
+                          const left = `${index * widthNum}%`;
+                          return (
+                            <BookingBox
+                              booking={booking}
+                              left={left}
+                              width={bookingWidth}
+                              onClick={handleBookingBoxClick}
+                              key={booking._id}
+                            />
+                          );
+                        })}
+                      </Box>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))}
+
           {loading && (
             <Box
               sx={{
                 position: "absolute",
-                bottom: 0,
-                right: 0,
-                width: isMobile
+                width: locationsLoading
+                  ? "100%"
+                  : isMobile
                   ? "calc(100% - 36px)"
                   : isTablet
                   ? "calc(100% - 42px)"
                   : "calc(100% - 48px)",
-                height: "100%",
+                height: locationsLoading ? "600px" : "100%",
                 display: "grid",
                 placeItems: "center",
                 bgcolor: "rgba(255, 255, 255, 0.3)",
                 borderRadius: 2,
+                ...(locationsLoading
+                  ? {
+                      top: 0,
+                      left: 0,
+                    }
+                  : {
+                      bottom: 0,
+                      right: 0,
+                    }),
               }}
             >
               <Loader />
